53e7b77cf5324ce5ee698dd2c0ed146feda82d91
radioactiveBrainstorm <55724365+radioactiveBrainstorm@users.noreply.github.com>
2019-09-27 20:49:00 -0400

    Meeting Updates 20190927

--- main.ts
+++ main.ts
@@ -1,1 +1,67 @@
-console.log("hello")
+let currentTime = 0
+let motoraAdjustment = 0
+let armPos = 0
+let loopStart = 0
+let lastArmPos = 0
+let startOffset = 0
+let increment = 0
+function armDown() {
+    motors.mediumA.run(-20, startOffset, MoveUnit.MilliSeconds)
+    lastArmPos = motors.mediumA.angle()
+    brick.showValue("lastArmPosInit", lastArmPos, 4)
+    pause(100)
+    while (true) {
+        motors.mediumA.run(-20, increment, MoveUnit.MilliSeconds)
+        armPos = motors.mediumA.angle()
+        brick.showValue("lastArmPos", lastArmPos, 1)
+        brick.showValue("armPos", armPos, 2)
+        brick.showValue("Difference", armPos - lastArmPos, 3)
+        if (lastArmPos - armPos == 0) {
+            music.playSoundEffect(sounds.communicationBravo)
+            break;
+        }
+        lastArmPos = armPos
+    }
+}
+function armUp() {
+    motors.mediumA.run(20, startOffset, MoveUnit.MilliSeconds)
+    lastArmPos = motors.mediumA.angle()
+    brick.showValue("lastArmPosInit", lastArmPos, 4)
+    pause(100)
+    while (true) {
+        motors.mediumA.run(20, increment, MoveUnit.MilliSeconds)
+        armPos = motors.mediumA.angle()
+        brick.showValue("lastArmPos", lastArmPos, 1)
+        brick.showValue("armPos", armPos, 2)
+        brick.showValue("Difference", armPos - lastArmPos, 3)
+        if (lastArmPos - armPos == 0) {
+            music.playSoundEffect(sounds.communicationBravo)
+            break;
+        }
+        lastArmPos = armPos
+    }
+}
+increment = 10
+startOffset = 100
+armUp()
+motors.largeBC.steer(0, 50, 2.25, MoveUnit.Rotations)
+loopStart = control.millis()
+while (currentTime - loopStart < 1600) {
+    motoraAdjustment = sensors.color1.light(LightIntensityMode.Reflected) - 40
+    motors.largeBC.steer(motoraAdjustment, 25)
+    currentTime = control.millis()
+}
+motors.largeBC.stop()
+motors.largeBC.tank(21, -21, 0.1, MoveUnit.Rotations)
+motors.largeBC.steer(0, 35, 0.35, MoveUnit.Rotations)
+armDown()
+motors.largeBC.steer(0, -25, 0.4, MoveUnit.Rotations)
+armUp()
+motors.largeBC.steer(0, -25, 0.1, MoveUnit.Rotations)
+motors.largeBC.tank(-5, 5, 0.05, MoveUnit.Rotations)
+armDown()
+motors.largeBC.steer(0, 25, 0.2, MoveUnit.Rotations)
+armUp()
+forever(function () {
+	
+})
